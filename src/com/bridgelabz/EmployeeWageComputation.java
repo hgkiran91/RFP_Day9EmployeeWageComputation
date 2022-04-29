e15d539a50f80c65047cd05ec068579526222e4e
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-04-29 09:06:05 +0530

    Added: UC4

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -4,6 +4,8 @@ public class EmployeeWageComputation {
     public static final int WAGE_PER_HOUR = 20;
     public static final int FULL_DAY_HOUR = 8;
     public static final int PART_TIME_HOUR = 4;
+    public static final int IS_FULL_DAY_PRESENT = 1;
+    public static final int IS_HALF_DAY_PRESENT = 2;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
@@ -12,17 +14,17 @@ public class EmployeeWageComputation {
         int empHalfDayPresent = 2;
         int dailyWage = 0;
         int empCheck = (int) (Math.random() * 10) % 3;
-        if (empCheck == empPresent) {
-            System.out.println("Employee Present");
-            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-            System.out.println("Daily Wage of an Employee: " + dailyWage);
-        } else if (empCheck == empHalfDayPresent) {
-            System.out.println("Employee Half Day Present");
-            dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-            System.out.println("Half Day Wage of an Employee: " + dailyWage);
-        } else {
-            System.out.println("Employee Absent");
-            System.out.println("Daily Wage of an Employee: " + dailyWage);
+
+        switch (empCheck) {
+            case IS_FULL_DAY_PRESENT:
+                dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                break;
+            case IS_HALF_DAY_PRESENT:
+                dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                break;
+            default:
+                dailyWage = 0;
         }
+        System.out.println(empCheck + " Daily Wage of an Employee: " + dailyWage);
     }
 }
