9e4a8d59c6fe20e80961b5a8a481c60c89ba07c8
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-04-29 09:10:05 +0530

    Added: UC5

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -6,6 +6,7 @@ public class EmployeeWageComputation {
     public static final int PART_TIME_HOUR = 4;
     public static final int IS_FULL_DAY_PRESENT = 1;
     public static final int IS_HALF_DAY_PRESENT = 2;
+    public static final int MAX_WORKING_DAYS_PER_MONTH = 20;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
@@ -13,18 +14,25 @@ public class EmployeeWageComputation {
         int empPresent = 1;
         int empHalfDayPresent = 2;
         int dailyWage = 0;
-        int empCheck = (int) (Math.random() * 10) % 3;
+        int totalEmpWage=0;
 
-        switch (empCheck) {
-            case IS_FULL_DAY_PRESENT:
-                dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                break;
-            case IS_HALF_DAY_PRESENT:
-                dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-                break;
-            default:
-                dailyWage = 0;
+        int totalWorkingDays = 0;
+        while (totalWorkingDays < MAX_WORKING_DAYS_PER_MONTH) {
+            int empCheck = (int) (Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_DAY_PRESENT:
+                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    break;
+                case IS_HALF_DAY_PRESENT:
+                    dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                    break;
+                default:
+                    dailyWage = 0;
+            }
+            totalWorkingDays++;
+            totalEmpWage+=dailyWage;
+            System.out.println(empCheck + " Daily Wage of an Employee: " + dailyWage);
         }
-        System.out.println(empCheck + " Daily Wage of an Employee: " + dailyWage);
+        System.out.println("Total Employee Wage for a Month: "+totalEmpWage);
     }
 }
